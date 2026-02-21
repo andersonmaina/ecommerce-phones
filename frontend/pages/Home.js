40e8926945462070a86e9cdfb38196c8b690ac1a
Anderson Maina <anderson.njoroge22@students.dkut.ac.ke>
2026-02-21 21:14:07 +0300

    Initial push of frontend files including React setup, store, components, pages, and CSS styles

---
+++ frontend/pages/Home.js
@@ -0,0 +1,14 @@
+// Sample page - Home.js
+import React from 'react';
+import Button from '../components/Button';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to E-commerce Phones</h1>
+            <Button label="Shop Now" onClick={() => {}} />
+        </div>
+    );
+};
+
+export default Home;
